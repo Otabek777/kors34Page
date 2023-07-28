a9096845fdc1362626c70e6e61859cfec43849ee
Otabek <53979293+Otabek777@users.noreply.github.com>
2023-07-28 12:53:45 +0500

    финиш правки

--- js/main.js
+++ js/main.js
@@ -13,6 +13,20 @@ $(".header__burgir").click(function() {
         },300);
     };
 });
+if($(".modal-open")) {
+    $(".modal-open").click(function() {
+        $(".modal").addClass("active");
+        setTimeout(function() {
+            $(".modal").addClass("open");
+        },10);
+    });
+    $(".modal__close").click(function() {
+        $(".modal").removeClass("open");
+        setTimeout(function() {
+            $(".modal").removeClass("active");
+        },300);
+    });
+};
 if($(".counter")) {
     var quantity = $(".counter");
     quantity.children(".plus").click(function() {
@@ -40,17 +54,58 @@ if($(".product__tabBtn")) {
     TabClick("1");
     TabClick("2");
 };
-$(".filter-open").click(function() {
-    $(".aside__close").addClass("active");
-    $(".aside").addClass("active");
-    setTimeout(function() {
-        $(".aside").addClass("open");
-    },10);
-});
-$(".filter-close").click(function() {
-    $(".aside__close").removeClass("active");
-    $(".aside").removeClass("open");
-    setTimeout(function() {
-        $(".aside").removeClass("active");
-    },300);
-});
+if($(".filter-open")) {
+    $(".filter-open").click(function() {
+        $(".aside__close").addClass("active");
+        $(".aside").addClass("active");
+        setTimeout(function() {
+            $(".aside").addClass("open");
+        },10);
+    });
+    $(".filter-close").click(function() {
+        $(".aside__close").removeClass("active");
+        $(".aside").removeClass("open");
+        setTimeout(function() {
+            $(".aside").removeClass("active");
+        },300);
+    });
+}
+
+if(document.querySelector('.input_phone')) {
+    window.addEventListener("DOMContentLoaded", function() {
+        [].forEach.call( document.querySelectorAll('.tel'), function(input) {
+        var keyCode;
+        function mask(event) {
+            event.keyCode && (keyCode = event.keyCode);
+            var pos = this.selectionStart;
+            if (pos < 3) event.preventDefault();
+            var matrix = "+7 (___) ___ ____",
+                i = 0,
+                def = matrix.replace(/\D/g, ""),
+                val = this.value.replace(/\D/g, ""),
+                new_value = matrix.replace(/[_\d]/g, function(a) {
+                    return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+                });
+            i = new_value.indexOf("_");
+            if (i != -1) {
+                i < 5 && (i = 3);
+                new_value = new_value.slice(0, i)
+            }
+            var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+                function(a) {
+                    return "\\d{1," + a.length + "}"
+                }).replace(/[+()]/g, "\\$&");
+            reg = new RegExp("^" + reg + "$");
+            if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+            if (event.type == "blur" && this.value.length < 5)  this.value = ""
+        }
+    
+        input.addEventListener("input", mask, false);
+        input.addEventListener("focus", mask, false);
+        input.addEventListener("blur", mask, false);
+        input.addEventListener("keydown", mask, false)
+    
+      });
+    
+    });
+};
